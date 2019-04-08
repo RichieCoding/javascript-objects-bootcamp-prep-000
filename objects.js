1095c11c24d018cb30112dc75ce43739e781d1a7
Richard Li <richardli125@gmail.com>
2019-04-08 04:16:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,5 +5,3 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName]: songTitle})
 }
-
-updatePlaylist(playlist, 'bruno mars', 'Just the way you are')
