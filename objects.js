3fa6d0d42465f8184046a83f12829c2d0fb793c3
Richard Li <richardli125@gmail.com>
2019-04-08 04:16:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,9 @@
 var playlist = {
-  Daniel: 'Japenese Denim'
+  ['Daniel Caesar']:  'Japenese Denim'
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName]: songTitle})
 }
+
+updatePlaylist(playlist, 'bruno mars', 'Just the way you are')
